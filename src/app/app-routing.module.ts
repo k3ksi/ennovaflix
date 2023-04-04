92945eb0c9209afbe44424790c2993c7c627284a
Andrea <andrea98186@gmail.com>
2023-04-04 18:00:28 +0200

    feat: modifica css e creazione search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotfoundPageComponent } from './components/pages/notfound-page/notfound-page.component';
 import { VideoPageComponent } from './components/pages/video-page/video-page.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { SearchComponent } from './components/pages/search/search.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'home', redirectTo: '' },
+  { path: 'cerca', component: SearchComponent},
   { path: 'film', children:[{
     path: ':slug',
     component: VideoPageComponent
