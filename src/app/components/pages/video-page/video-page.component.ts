9d42c543dafc9bc2820b65ff87fdd8c6af60504f
salvo720 <salvoccelano91gmail.com>
2023-04-07 10:43:11 +0200

    feat(angular) : aggiornato componente di dettaglio dei video , adesso funziona con i dati recuperati dal service , aggiornato il testo sul componente dei preferiti , aggiunta interfaccia Irecenzione per tipizzare le recenzioni

--- src/app/components/pages/video-page/video-page.component.ts
+++ src/app/components/pages/video-page/video-page.component.ts
@@ -1,82 +1,92 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../../../services/video/video.service';
+import IResponseVideo from 'src/app/interfaces/IResponseVideo/iresponse-video';
+import IRecenzioni from 'src/app/interfaces/IResponseVideo/IRecenzioni';
 
 @Component({
   selector: 'app-video-page',
   templateUrl: './video-page.component.html',
   styleUrls: ['./video-page.component.scss'],
 })
-export class VideoPageComponent {
+export class VideoPageComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private videoService = inject(VideoService);
 
-  @Input() details = {
-    titolo: 'Titolo',
-    descrizione:
-      'Film del 2011 molto avvincente bellissimo ti' +
-      ' spenniamo i soldi non ci crederai mai guarda qui sei un pollo arrosto ahahaha',
-    rating: 4.5,
-    cast: ['Leonardo Di Caprio', 'Sharon Stone'],
-    anno: 2011,
-    isSerie: true,
-    stagioni: [
-      {
-        numero_stagione: 1,
-        numero_puntate: 5
-      },
-      {
-        numero_stagione: 2,
-        numero_puntate: 7
-      }
-    ],
-    recensioni: [
-      {
-        nickname: 'Elena',
-        rating: 4,
-        descrizione: 'Molto bello',
-      },
-      {
-        nickname: 'Marco',
-        rating: 2,
-        descrizione: 'Non mi è piaciuto',
-      },
-      {
-        nickname: 'Marta',
-        rating: 5,
-        descrizione: 'Il mio preferito!',
-      },
-    ],
-  };
+  details!: IResponseVideo
 
-  constructor() {
-    console.log(this.route)
-    this.route.params.subscribe(params => {
-      const id = params['slug'];
-      const isFilmOrSerieTv:string = window.location.href.split('/').reduce((acc:string,urlsegment:string , index:number ) => {
+  // @Input() details = {
+  //   titolo: 'Titolo',
+  //   descrizione:
+  //     'Film del 2011 molto avvincente bellissimo ti' +
+  //     ' spenniamo i soldi non ci crederai mai guarda qui sei un pollo arrosto ahahaha',
+  //
+  //   anno: 2011,
+  //   isSerie: true,
+  // };
 
-        if( urlsegment.trim() == params['slug']) {
-           console.log(urlsegment ,index);
-           console.log(window.location.href.split('/'));
-           return String(index-1)
-         }
-         return acc
-      })
+  recenzioni: Array<IRecenzioni> = [
+    {
+      nickname: 'Elena',
+      rating: 4,
+      descrizione: 'Molto bello',
+    },
+    {
+      nickname: 'Marco',
+      rating: 2,
+      descrizione: 'Non mi è piaciuto',
+    },
+    {
+      nickname: 'Marta',
+      rating: 5,
+      descrizione: 'Il mio preferito!',
+    },
+  ];
 
-      console.log(isFilmOrSerieTv);
-      if(isFilmOrSerieTv=="film"){
-        console.log("film " ,this.videoService.getFilm(id));
-      }else if(isFilmOrSerieTv=="serietv"){
-        console.log("serietv " ,this.videoService.getFilm(id));
-      }
-    });
+  cast = ['Leonardo Di Caprio', 'Sharon Stone'];
+  rating = 4.5;
+  stagioni = [
+    {
+      numero_stagione: 1,
+      numero_puntate: 5
+    },
+    {
+      numero_stagione: 2,
+      numero_puntate: 7
+    }
+  ];
 
-  }
   write: boolean = false;
   puntate: number[] = []
   ratingValue = 0
+
+  ngOnInit(): void {
+    this.getFilmOrSerieTv();
+  }
+
   onSelect(val: number) {
     this.puntate = []
-    this.puntate = Array.from({ length: this.details.stagioni[val - 1].numero_puntate }, (_, i) => i + 1);
+    this.puntate = Array.from({ length: this.stagioni[val - 1].numero_puntate }, (_, i) => i + 1);
+  }
+
+  getFilmOrSerieTv(): void {
+    this.route.params.subscribe(params => {
+      const id = params['slug'];
+      const isFilmOrSerieTv: string = window.location.href.split('/').reduce((acc: string, urlsegment: string, index: number, array: string[]) => {
+
+        if (urlsegment.trim() == params['slug']) {
+          const isFilmOrSerieTvIndex = index - 1;
+          return array[isFilmOrSerieTvIndex];
+        }
+        return "";
+      });
+      // console.log("isFilmOrSerieTv: ", isFilmOrSerieTv);
+      if (isFilmOrSerieTv == "") {
+        throw new Error("si e verificato un errore ");
+      }
+      // se e un film richiamo il metodo dei film altrimenti sara una serieTV
+      this.details = (isFilmOrSerieTv == "film") ? this.videoService.getFilm(id) : this.videoService.getSerieTV(id);
+    });
+
   }
 }
---
+++ src/app/interfaces/IResponseVideo/IRecenzioni.ts
@@ -0,0 +1,5 @@
+export default interface IRecenzioni {
+  nickname: string
+  rating: number
+  descrizione: string
+}
